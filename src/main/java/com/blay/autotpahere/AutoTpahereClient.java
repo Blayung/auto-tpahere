952fb2b969988306167099cfc6add11808330fe2
Blayung <gpewojan1@gmail.com>
2022-09-17 20:52:09 +0200

    Add 1.1 seconds delay between commands.

--- src/main/java/com/blay/autotpahere/AutoTpahereClient.java
+++ src/main/java/com/blay/autotpahere/AutoTpahereClient.java
@@ -11,14 +11,47 @@ import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.network.ClientPlayerEntity;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Thread;
+import java.lang.InterruptedException;
+import java.util.concurrent.TimeUnit;
 
 public class AutoTpahereClient implements ClientModInitializer {
     public static KeyBinding allTpaHereKeybind;
 
+    private static int count;
+    private static ClientPlayNetworkHandler networkHandler;
+    private static Collection<PlayerListEntry> playerList;
+    private static ClientPlayerEntity clientPlayer;
+    private static String name;
+    private static List<String> names;
+    private static boolean isRunning;
+    private static Thread thread;
+
+    private class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            for(int i=0;i<count;i++){
+                clientPlayer.sendMessage(Text.literal("Trying /tpahere "+names.get(i)+"..."));
+                clientPlayer.sendCommand("tpahere "+names.get(i),null);
+                try{
+                    TimeUnit.MILLISECONDS.sleep(1100);
+                }catch(InterruptedException e){
+                    isRunning=false;
+                    clientPlayer.sendMessage(Text.literal("Something went wrong..."));
+                }
+            }
+            isRunning=false;
+        }
+    }
+
     @Override
     public void onInitializeClient() {
+        isRunning=false;
         allTpaHereKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "auto-tpahere.key.tpa",
             InputUtil.Type.KEYSYM,
@@ -28,20 +61,23 @@ public class AutoTpahereClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if(allTpaHereKeybind.wasPressed()) {
-                int count=0;
-                ClientPlayNetworkHandler networkHandler=MinecraftClient.getInstance().getNetworkHandler();
-                Collection<PlayerListEntry> playerList=networkHandler.getPlayerList();
+                if(isRunning){
+                    client.player.sendMessage(Text.literal("Global /tpahere is already in progress!"),true);
+                }else{
+                    isRunning=true;
+                    thread = new Thread(new MyRunnable());
+                    networkHandler=MinecraftClient.getInstance().getNetworkHandler();
+                    playerList=networkHandler.getPlayerList();
+                    clientPlayer=client.player;
 
-                for(PlayerListEntry player:playerList){
-                    String name=player.getProfile().getName();
-                    if(client.player.getEntityName()!=name){
+                    count=0;
+                    names=new ArrayList<String>();
+                    for(PlayerListEntry player:playerList){
+                        name=player.getProfile().getName();
                         count++;
-                        client.player.sendMessage(Text.literal("Trying /tpahere "+name+" ..."));
-                        client.player.sendCommand("tpahere "+name,null);
+                        names.add(name);
                     }
-                }
-                if(count==0){
-                    client.player.sendMessage(Text.literal("You are the only one on the server!"),true);
+                    thread.start();
                 }
             }
         });
